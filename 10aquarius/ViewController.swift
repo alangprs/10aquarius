1cdf35117ef26a2e8f922b74ad6958bf25a706b2
ericwugo <ericwugo@gmail.com>
2020-08-01 13:54:42 +0800

    Initial Commit

---
+++ 10aquarius/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  10aquarius
+//
+//  Created by user on 2020/8/1.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
